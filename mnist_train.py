6b8502414dcdb05f035ff67a5b52fd99a4f90e59
arthurlaurens <alvrooijen@gmail.com>
2016-12-05 11:19:13 +0100

    Cleanup training

--- mnist_train.py
+++ mnist_train.py
@@ -1,24 +1,3 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""Simple, end-to-end, LeNet-5-like convolutional MNIST model example.
-
-This should achieve a test error of 0.7%. Please keep this model as simple and
-linear as possible, it is meant as a tutorial for simple convolutional models.
-Run with --self_test on the command line to execute a short self-test.
-"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -33,35 +12,48 @@ import mnist
 import mnist_input as input
 import mnist_eval
 
+# Settings
+CKPT_DIR = '/home/s1259008/research_project/tmp/mnist/ckpts/'
+
+SUMMARY_FREQ = 10
+EVAL_FREQ = 1000
+SAVE_FREQ = 1000
+
+N_EPOCHS = 10
+BATCH_SIZE = 64
+DROPOUT_RATE = 0.5
+
+NUM_LABELS = 10
+IMAGE_SIZE = 28
+NUM_CHANNELS = 1
 
 
+def generate_experiment_id(conv_settings, full_settings):
+  date_time = time.strftime("%d-%b-%Y_%H-%M-%S", time.gmtime())
 
-def generate_train_dir(conv_sizes, local_sizes):
-  dir_name = time.strftime("%d-%b-%Y_%H-%M-%S_", time.gmtime())
+  # Add conv layer settings
+  layer_settings = 'C'
+  for setting in conv_settings:
+    layer_settings += '-{:d}'.format(setting)
 
-  # add kernel layer data
-  dir_name += 'K'
-  for size in conv_sizes:
-    str = '-%d' % size
-    dir_name += str
+  # Add full layer settings
+  layer_settings += '-F'
+  for setting in full_settings:
+    layer_settings += '-{:d}'.format(setting)
 
-  # add local layer data
-  dir_name += '-L'
-  for size in local_sizes:
-    str = '-%d' % size
-    dir_name += str
+  return layer_settings + '_' + date_time
 
-  dir_path = mnist.CHECKPOINT_DIR + dir_name
-  if not tf.gfile.Exists(dir_path):
-    tf.gfile.MakeDirs(dir_path)
 
-  return dir_path
+def create_experiment_dir(path):
+  if not tf.gfile.Exists(path):
+    tf.gfile.MakeDirs(path)
 
 
 def calc_total_params():
+  """From: http://stackoverflow.com/a/38161314/2351350"""
   total_parameters = 0
   for variable in tf.trainable_variables():
-    # shape is an array of tf.Dimension
+    # Shape is an array of tf.Dimension
     shape = variable.get_shape()
     print('v shape %s' % shape)
     variable_parametes = 1
@@ -70,106 +62,97 @@ def calc_total_params():
     print('v params %d' % variable_parametes)
     total_parameters += variable_parametes
   print('total params %d' % total_parameters)
-  return
 
 
-def train(convl_settings, dense_settings):
-  print("Training model convl:" + str(convl_settings) + ' dense:' + str(dense_settings))
-
+def train(conv_settings, full_settings):
+  print("Training model convl:" + str(conv_settings) + ' dense:' + str(full_settings))
 
   with tf.Graph().as_default():
     global_step = tf.Variable(0, dtype=mnist.data_type(), trainable=False)
 
-    # train inference
-    train_images_input = tf.placeholder(mnist.data_type(),
-                                        shape=(mnist.BATCH_SIZE,
-                                               mnist.IMAGE_SIZE,
-                                               mnist.IMAGE_SIZE,
-                                               mnist.NUM_CHANNELS))
-    logits = mnist.model(train_images_input,
-                         convl_settings,
-                         dense_settings,
-                         mnist.NUM_LABELS,
-                         True)  # training model
-    train_prediction = tf.nn.softmax(logits)
+    # Inputs
+    images_input = tf.placeholder(mnist.data_type(),
+                                  shape=(BATCH_SIZE,
+                                         IMAGE_SIZE,
+                                         IMAGE_SIZE,
+                                         NUM_CHANNELS))
+    labels_input = tf.placeholder(tf.int64, shape=(BATCH_SIZE,))
+    dropout_pl = tf.placeholder(tf.float32)
 
-    # train loss
-    train_labels_input = tf.placeholder(tf.int64, shape=(mnist.BATCH_SIZE,))
-    loss = mnist.loss(logits, train_labels_input)
+    # Inference
+    logits = mnist.model(images_input, conv_settings, full_settings, NUM_LABELS, dropout_pl)
+    prediction = tf.nn.softmax(logits)
 
-    # train apply gradients
+    # Loss
+    loss = mnist.loss(logits, labels_input)
+
+    # Train node
     train_op = mnist.train(loss, global_step)
-
-    # validation inference
-    validation_images_input = tf.placeholder(mnist.data_type(),
-                                             shape=(mnist.BATCH_SIZE,
-                                                    mnist.IMAGE_SIZE,
-                                                    mnist.IMAGE_SIZE,
-                                                    mnist.NUM_CHANNELS))
-    validation_prediction = tf.nn.softmax(mnist.model(validation_images_input,
-                                                      convl_settings,
-                                                      dense_settings,
-                                                      mnist.NUM_LABELS,
-                                                      False))
 
     # Setup saver and related vars
     saver = tf.train.Saver(max_to_keep=None)
-    # create dir for ckpts
-    train_dir = generate_train_dir(convl_settings, dense_settings)
-    ckpt_path = train_dir + '/mnist.ckpt'
+    # Create dir for ckpts
+    experiment_id = generate_experiment_id(conv_settings, full_settings)
+    experiment_dir = CKPT_DIR + experiment_id + '/'
+    create_experiment_dir(experiment_dir)
+    ckpt_path = experiment_dir + 'mnist.ckpt'
 
     # Setup summary writer for Tensorboard
     merged = tf.merge_all_summaries()
 
-    # Get the data.
+    # Get data.
     train_images, train_labels, validation_images, validation_labels = input.data(True)
     train_size = train_labels.shape[0]
 
-    #  train
+    #  Train
     with tf.Session() as sess:
       tf.initialize_all_variables().run()
-      summary_writer = tf.train.SummaryWriter(
-        mnist.TENSORBOARD_DIRECTORY + '/train',
-        sess.graph)
+      summary_writer = tf.train.SummaryWriter(CKPT_DIR + 'train', sess.graph)
 
-      n_steps = int(mnist.NUM_EPOCHS * train_size) // mnist.BATCH_SIZE
+      n_steps = int(N_EPOCHS * train_size) // BATCH_SIZE
       for step in xrange(n_steps):
-        # Compute the offset of the current minibatch in the data.
-        # Note that we could use better randomization across epochs.
-        offset = (step * mnist.BATCH_SIZE) % (train_size - mnist.BATCH_SIZE)
-        batch_data = train_images[offset:(offset + mnist.BATCH_SIZE), ...]
-        batch_labels = train_labels[offset:(offset + mnist.BATCH_SIZE)]
-        # This dictionary maps the batch data (as a numpy array) to the
-        # node in the graph it should be fed to.
-        feed_dict = {train_images_input: batch_data,
-                     train_labels_input: batch_labels}
+        # Train
+        # Generate batch
+        offset = (step * BATCH_SIZE) % (train_size - BATCH_SIZE)
+        batch_data = train_images[offset:(offset + BATCH_SIZE), ...]
+        batch_labels = train_labels[offset:(offset + BATCH_SIZE)]
+        # Update net
+        feed_dict = {images_input: batch_data, labels_input: batch_labels, dropout_pl: DROPOUT_RATE}
+        _, l, predictions, summary = sess.run([train_op, loss, prediction, merged], feed_dict=feed_dict)
 
-        # train
-        _, l, predictions, summary = sess.run([train_op, loss, train_prediction, merged],
-                                     feed_dict=feed_dict)
-
-        # Print validation error and save summaries
-        if step % mnist.EVAL_FREQUENCY == 0:
+        # Validate
+        if step % EVAL_FREQ == 0:
           predictions = mnist_eval.eval_in_batches(validation_images,
                                                    sess,
-                                                   validation_images_input,
-                                                   validation_prediction)
+                                                   images_input,
+                                                   prediction,
+                                                   dropout_pl)
           validation_error = mnist.error_rate(predictions, validation_labels)
-          print('Validation error: %.2f%% after %d/%d steps' %
-                (validation_error, step, n_steps))
+          print('Validation error: %.2f%% after %d/%d steps' % (validation_error, step, n_steps))
 
-        summary_writer.add_summary(summary, step)
+        # Summaries
+        if step % SUMMARY_FREQ == 0:
+          summary_writer.add_summary(summary, step)
 
-        # Save variables
-        if step % (n_steps // mnist.SAVE_FREQUENCY) == 0 or step + 1 is n_steps:
+        # Save
+        if step % SAVE_FREQ == 0 or step + 1 == n_steps:
           ckpt_file_path = saver.save(sess, ckpt_path, global_step=step)
           print('Saved checkpoint file: %s' % ckpt_file_path)
 
-      # Print test error
-      mnist_eval.eval_once(ckpt_file_path)
+      # Evaluate
+      eval_images, eval_labels = input.data(False)
+      predictions = mnist_eval.eval_in_batches(eval_images, sess, images_input, prediction, dropout_pl)
+      validation_error = mnist.error_rate(predictions, validation_labels)
+      print('Validation error: %.2f%% after %d/%d steps' % (validation_error, step, n_steps))
 
 
 if __name__ == '__main__':
+  """
+  Usage, one of the following:
+  python train.py n_conv_1 n_conv_2 n_full_1
+  python train.py n_conv_1 n_full_1
+  """
+
   n_args = len(sys.argv)
   if n_args is 3:
     convl = [int(sys.argv[1])]
