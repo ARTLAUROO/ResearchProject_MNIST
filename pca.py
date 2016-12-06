834cf47bb159520426573b06dffb259ad499527c
Arthur van Rooijen <alvrooijen@gmail.com>
2016-12-06 14:16:16 +0100

    Remove redundant code in pca.py

--- pca.py
+++ pca.py
@@ -7,16 +7,12 @@ from six.moves import xrange  # pylint: disable=redefined-builtin
 import sys
 import os
 
-import matplotlib.pyplot as plt
-from matplotlib.legend_handler import HandlerLine2D
-
 import tensorflow as tf
 
 import numpy as np
 from sklearn.decomposition import PCA
 
 import model
-import input
 import eval
 import train
 
@@ -92,40 +88,6 @@ def pca_reduction(weights, biases, n_components):
   return weights, biases, cumsum[-1]
 
 
-def generate_pca_plot(ckpt_path):
-  """
-  Given a checkpoint file generates a pca plot for it.
-  :param ckpt_path: absolute or relative path to the checkpoint file. Must look
-  like this: ../../<EXPERIMENT_ID>/<CHECKPOINT_FILE>
-  :return:
-  """
-  print('Generating plot for %s' % ckpt_path)
-
-  settings = ckpt_path.split('/')
-  experiment_id_str = settings[-2]  # dir name of the checkpoint file
-  file_name = settings[-1]
-
-  if not os.path.exists(model.PLOT_DIR + experiment_id_str):
-    os.makedirs(model.PLOT_DIR + experiment_id_str)
-  plt.savefig(model.PLOT_DIR + experiment_id_str + '/' + file_name + '.jpg')
-
-  errors, cumsums = eval_pca(ckpt_path)
-
-  plt.figure()
-  plt.ylabel('Percentage')
-  plt.xlabel('N PCA components')
-  plt.axis([-0.5, len(errors), -2, 102])
-  plt.grid(True)
-  error_points, = plt.plot(errors, 'ro', label='Error on test set')
-  cumsum_points, = plt.plot(cumsums, 'bx', label='Kernel variance kept')
-
-  plt.legend(bbox_to_anchor=(0.9, 0.85),
-             bbox_transform=plt.gcf().transFigure,
-             handler_map={error_points: HandlerLine2D(numpoints=1),
-                          cumsum_points: HandlerLine2D(numpoints=1)})
-  # plt.show()
-
-
 def create_pca_ckpt(ckpt_path):
   """
   Given a checkpoint file it generates
@@ -184,197 +146,11 @@ def create_pca_ckpts(ckpt_dir_path):
   print('Evaluating DIR: ' + ckpt_dir_path)
   ckpts = []
   for f in os.listdir(ckpt_dir_path):
-    if '.ckpt' in f and not '.meta' in f:
+    if '.ckpt' in f and '.meta' not in f:
       ckpts.append(path + f)
 
   for ckpt in sorted(ckpts):
     create_pca_ckpt(ckpt)
-
-
-# TODO
-def generate_pca_plots():
-  train_dirs = [x[0] for x in os.walk(train.CKPT_DIR)]
-  train_dirs = train_dirs[1:]  # skip base dir
-
-  for train_dir in train_dirs:
-    train_checkpoints = os.listdir(train_dir)
-
-    for train_ckpt in train_checkpoints:
-      if 'meta' in train_ckpt or 'checkpoint' in train_ckpt:
-        continue
-      generate_pca_plot(train_dir + '/' + train_ckpt)
-
-
-# TODO
-def eval_pca(ckpt_path):
-  errors = []
-  cumsums = []
-
-  with tf.Graph().as_default():
-    # Load settings from dir name
-    dir_name = ckpt_path.split('/')
-    dir_name = dir_name[-2]  # drop path prefix
-    convl_settings, dense_settings = eval.get_settings_from_experiment_id(
-      dir_name)
-
-    # Construct inference
-    data_batch = tf.placeholder(model.data_type(),
-                                shape=(model.BATCH_SIZE,
-                                       model.IMAGE_SIZE,
-                                       model.IMAGE_SIZE,
-                                       model.N_CHANNELS))
-    logits = model.inference(data_batch,
-                             convl_settings,
-                             dense_settings,
-                             model.NUM_LABELS,
-                             False)  # eval inference
-
-    prediction = tf.nn.softmax(logits)
-
-    saver = tf.train.Saver()
-
-    merged = tf.merge_all_summaries()
-
-    with tf.Session() as sess:
-      saver.restore(sess, ckpt_path)
-
-      # apply PCA
-      with tf.variable_scope('conv1') as scope:
-        scope.reuse_variables()
-
-        variable_w = tf.get_variable('weights')
-        variable_b = tf.get_variable('biases')
-
-        original_w = variable_w.eval()
-        original_b = variable_b.eval()
-
-        settings = ckpt_path.split('/')
-        dir_name = settings[-2]
-
-        # Plot original kernel weights
-        # array = to_img_shaped(original_w, original_b)
-        # for i in xrange(len(array)):
-        #     plt.imshow(array[i], interpolation='none')
-        #     # plt.show()
-        #     file_name = 'original_w-' + str(i)
-        #     plt.savefig(mnist.PLOT_DIR + dir_name + '/' + file_name + '.jpg')
-
-
-
-        # pca_range = original_w.shape[-1]
-        pca_range = 25
-
-        data, labels = input.data(False)
-        for i in xrange(pca_range):
-          writer = tf.train.SummaryWriter(
-            model.TENSORBOARD_DIRECTORY + '/pca_components-' + str(i),
-            sess.graph)
-
-          n_components = i + 1
-
-          weights, biases, cumsum = pca_reduction(original_w,
-                                                  original_b,
-                                                  n_components)
-
-          # plot adjusted weights
-          # array = to_img_shaped(weights, biases)
-          # for j in xrange(len(array)):
-          #     plt.imshow(array[j], interpolation='none')
-          #     # plt.show()
-          #     file_name = 'pca-' + str(i) + '_w-' + str(j)
-          #     plt.savefig(mnist.PLOT_DIR + dir_name + '/' + file_name + '.jpg')
-
-          # plot passed thourgh values
-          summary = sess.run(merged,
-                             feed_dict={data_batch: data[:64, ...]})
-          writer.add_summary(summary, i)
-
-          sess.run(variable_w.assign(weights))
-          sess.run(variable_b.assign(biases))
-          cumsums.append(cumsum)
-
-          print("PCA:%d" % i)
-          predictions = eval.eval_in_batches(data,
-                                             sess,
-                                             data_batch,
-                                             prediction)
-          error = model.error_rate(predictions, labels)
-          print('PCA %d error: %.2f%%' % (n_components, error))
-          errors.append(error)
-  return errors, cumsums
-
-
-# TODO
-def pca(ckpt_path, convs, locals):
-  print("PCA for ckpt file: %s" % ckpt_path)
-
-  with tf.Graph().as_default():
-    test_data, test_labels = input.data(False)
-
-    # Build up inference in order to load/save variables and apply pca
-    eval_data = tf.placeholder(1,
-                               shape=(model.BATCmnist.data_type(),
-                                      model.IMAGE_SIZE,
-                                      model.IMAGE_SIZE,
-                                      model.N_CHANNELS))
-    N_KERNELS_LAYER_1 = convs[0]
-    if len(convs) == 1:
-      logits = model.inference(eval_data,
-                               convs[0],
-                               None,
-                               locals[0],
-                               model.NUM_LABELS,
-                               False)  # eval inference
-    else:
-      logits = model.inference(eval_data,
-                               convs[0],
-                               convs[1],
-                               locals[0],
-                               model.NUM_LABELS,
-                               False)  # eval inference
-
-    # Predictions for the test and validation, which we'll compute less often.
-    eval_prediction = tf.nn.softmax(logits)
-
-    saver = tf.train.Saver(max_to_keep=None)
-
-    with tf.Session(config=tf.ConfigProto(log_device_placement=False)) as sess:
-      saver.restore(sess, ckpt_path)
-
-      # Print test error before pca
-      print("PCA:before")
-      eval.eval_ckpt(ckpt_path)
-
-      # pca
-      with tf.variable_scope('conv1') as scope:
-        scope.reuse_variables()
-
-        variable_w = tf.get_variable('weights')
-        variable_b = tf.get_variable('biases')
-
-        original_w = variable_w.eval()
-        original_b = variable_b.eval()
-
-        for i in xrange(1):
-          weights, biases = pca_reduction(original_w, original_b, i + 1)
-
-          sess.run(variable_w.assign(weights))
-          sess.run(variable_b.assign(biases))
-
-          print("PCA:%d" % i)
-          test_error = eval.run(sess,
-                                test_data,
-                                test_labels,
-                                eval_data,
-                                eval_prediction)
-
-          # # Save variables
-          # ckpt_path = mnist.CHECKPOINT_DIR + 'pca.ckpt'
-          # ckpt_path = saver.save(sess, ckpt_path)
-          # print('Saved checkpoint file: %s' % ckpt_path)
-          #
-          # # Print test error after pca
-          # mnist_eval.eval(ckpt_path)
 
 
 if __name__ == '__main__':
